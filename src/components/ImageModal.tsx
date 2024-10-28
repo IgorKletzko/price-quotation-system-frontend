abf48126238454b037bfd687f7a48f9f9564e61e
thailandSababa <thailand.sababa.operations@gmail.com>
2024-10-28 20:59:44 +0700

    a

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -18,7 +18,7 @@ const ImageModal: React.FC<ImageModalProps> = ({
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     if (event.target === event.currentTarget) {
-      onClose(); // close modal if the overlay (current target) was clicked
+      onClose();
     }
   };
 
@@ -27,19 +27,16 @@ const ImageModal: React.FC<ImageModalProps> = ({
       className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-75"
       onClick={handleOverlayClick}
     >
-      <div className="bg-white p-2 rounded-lg shadow-lg max-w-4xl max-h-full w-full h-4/5 sm:h-auto overflow-auto sm:p-8">
+      <div className="relative bg-white rounded-lg shadow-lg max-w-full w-full h-full sm:max-w-4xl sm:h-4/5 overflow-hidden">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-1 px-3 rounded"
         >
           X
         </button>
-        <ImageCarousel images={imageSrc}/>
-        {/* <img
-          src={imageSrc}
-          alt="Enlarged view"
-          className="w-full h-full object-cover sm:object-contain"
-        /> */}
+        <div className="w-full h-full p-2 sm:p-8 overflow-auto flex justify-center items-center">
+          <ImageCarousel images={imageSrc} />
+        </div>
       </div>
     </div>
   );
